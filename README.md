a91f5c69893a989141934e35cd58b1f25fd518a5
Brett Schellenberg <brett.schellenberg@gmail.com>
2024-01-02 10:06:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Front end interview boilerplate
+
+A lightly trimmed `npm create vite` repo to fork for interviews
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export const App = () => {
+  return (
+    <>
+      <h1>Brett Schellenberg</h1>
+      <h2 className="subdued">Interview</h2>
+    </>
+  );
+};
